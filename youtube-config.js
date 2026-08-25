7a0819cbb55a0bf18ca0f2c16ce20359e921c3ea
nxtcoreee3 <oliversvuxenkonto@gmail.com>
2026-08-25 21:38:25 +0200

    Add files via upload

---
+++ youtube-config.js
@@ -0,0 +1,8 @@
+/*
+ * Flux Websites configuration.
+ *
+ * Create a browser-restricted API key in Google Cloud with only the
+ * YouTube Data API v3 enabled, then paste it between the quotes below.
+ * Restrict the key to the exact domains where Flux is served.
+ */
+window.FLUX_YOUTUBE_API_KEY = '';
